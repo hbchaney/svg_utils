676a03a5635f7cbe8e175fccbf79ee50e9c2a421
Harrison Chaney <hbchaney@gmail.com>
2022-12-28 10:34:31 -0500

    added most of the math to BoundingBox

--- shape/bounding_box.py
+++ shape/bounding_box.py
@@ -1,23 +1,100 @@
 from edge import Edge
-
+from coordinate import Coordinate
 class BoundingBox : 
     
-    def __init__(self, edges: list[Edge]=None): 
+    def __init__(self, edges: list[Edge]=None, data_in : list[Coordinate,float] = None) -> None: 
         if edges is not None:
             self.__find_bounding_from_edges(edges) 
+            return
+        elif data_in is not None: 
+            self._midpoint = data_in[0] 
+            self._rx = data_in[1] 
+            self._ry = data_in[2] 
+            return 
         else:  
-            self._minx = None
-            self._miny = None 
-            self._maxx = None
-            self._maxy = None
+            self._midpoint = None
+            self._rx = None
+            self._ry = None 
         
-    def __find_bounding_from_edges(self,edges) -> None: 
+        
+    def __find_bounding_from_edges(self,edges : list[Edge]) -> None: 
         
         # initializing the edges 
         temp_minx = edges[0].start.x()
         temp_miny = edges[0].start.y()
         temp_maxx = edges[0].start.x()
         temp_maxy = edges[0].start.y() 
+        
+        #going through each edge 
+        for e in edges: 
+            #max x 
+            if e.start.x() >= temp_maxx: 
+                temp_maxx = e.start.x()
+            if e.end.x() >= temp_maxx: 
+                temp_maxx = e.end.x() 
+                
+            #min x 
+            if e.start.x() <= temp_minx: 
+                temp_minx = e.start.x() 
+            if e.end.x() <= temp_minx: 
+                temp_minx = e.end.x() 
+            
+            #max y 
+            if e.start.y() >= temp_maxy: 
+                temp_maxy= e.start.y() 
+            if e.end.y() >= temp_maxy: 
+                temp_maxy = e.end.y() 
+                
+            #min y 
+            if e.start.y() <= temp_miny: 
+                temp_miny = e.start.y() 
+            if e.end.y() <= temp_miny: 
+                temp_miny = e.end.y() 
+                
+        #### Calculating the mid point and rx + ry 
+        mid_x = (temp_maxx-temp_minx)/2 + temp_minx
+        mid_y = (temp_maxy-temp_miny)/2 + temp_miny
+        self._midpoint = Coordinate(mid_x,mid_y)
+        
+        rx = (temp_maxx - temp_minx) / 2
+        ry = (temp_maxy - temp_miny) / 2
+        
+        self._rx = rx
+        self._ry = ry 
+        
+        
+    
+    def set_bounding_box(self,edges : list[Edge]) -> None: 
+        self.__find_bounding_from_edges(edges) 
+        
+        
+    def get_data(self): 
+        return [self._midpoint,self._rx,self._ry].copy() 
+        
+    def intersection(self,other : BoundingBox) -> BoundingBox: 
+        temp = other.get_data()
+        
+        other_mid = temp[0] 
+        other_rx = temp[1] 
+        other_ry = temp[2] 
+        
+        #compare if the x and y are in range 
+        if abs(self._midpoint.x() - other_mid.x()) <= self._rx + other_rx and abs(self._midpoint.y() - other_mid.y()) <= self._ry + other_ry:
+            new_rx = (abs(self._midpoint.x() - other_mid.x()) - (self._rx + other_rx)) / 2
+            new_ry = (abs(self._midpoint.y() - other_mid.y()) - (self._ry + other_ry)) / 2 
+            
+            new_mid_x = ((self._midpoint.x() + other_mid.x()) / 2)
+            new_mid_y = ((self._midpoint.x() + other_mid.y()) / 2)
+            
+            return BoundingBox(data_in = [Coordinate(new_mid_x,new_mid_y),new_rx,new_ry])
+
+        else: 
+            return False 
+        
+        
+        
+        
+    
         
         
     
--- shape/shape.py
+++ shape/shape.py
@@ -2,14 +2,21 @@
 # shape with a combination of edges 
 from edge import Edge
 from bounding_box import BoundingBox
+
 class Shape : 
     
     def __init__(self,
-                 _edges : Edge,
+                 _edges : list[Edge],
                  _id : str) -> None:  
         self._edges = _edges 
         self._id = _id 
+        self.__get_bounding_box()
+         
+    def __get_bounding_box(self) -> None: 
+        self.bounding_box = BoundingBox()
+        self.bounding_box.set_bounding_box(self._edges)
         
-    @property
-    def set_bounding_box(self) -> BoundingBox: 
-        return 
+    def reset_bounding_box(self) -> None: 
+        #resets the new bounding box
+        self.bounding_box = BoundingBox(edges = self._edges) 
+        
