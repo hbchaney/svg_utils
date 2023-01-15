005e661f7a9fdf2d53bd35e12a0ad5606c1f0226
bxchaney <bxchaney@gmail.com>
2023-01-15 12:04:02 -0500

    finished inital work on svg reader, may still need further testing

--- file/svg_reader.py
+++ file/svg_reader.py
@@ -1,5 +1,4 @@
 import os
-import re
 import sys
 from typing import Tuple
 from xml.etree import ElementTree as ET
@@ -7,9 +6,11 @@ from xml.etree import ElementTree as ET
 __location__ = os.path.join(os.getcwd(), os.path.dirname(__file__))
 __parent_dir__ = os.path.realpath(os.path.join(__location__, ".."))
 
-sys.path.insert(0, __parent_dir__)
+if __parent_dir__ not in sys.path:
+    sys.path.insert(0, __parent_dir__)
 
-from shape import Shape, Coordinate
+from .path_reader import PathReader
+from shape import Shape
 
 class SvgReader:
     """
@@ -25,6 +26,7 @@ class SvgReader:
     def __init__(self) -> None:
         pass
     
+
     def get_shapes(self, svg_filepath: str) -> list[Shape]:
         """
         This method parses the provided filepath to a .svg file and returns
@@ -32,55 +34,38 @@ class SvgReader:
         """
         shape_arr = list()
         _root = ET.parse(svg_filepath).getroot()
-        for d in self._gen_next_path_attrib(_root):
-            shape_arr.append(self._parse_path(d))
+        for obj_id, d in self._gen_next_path_attrib(_root):
+            shape_arr.append(self._parse_path(obj_id, d))
         return shape_arr
 
-    def _gen_next_path_attrib(self, root: ET.Element) -> None:
+    def _gen_next_path_attrib(self, root: ET.Element) -> Tuple[str, str]:
         for child in root.findall("xmlns:g/xmlns:path", self._ns):
-            yield child.attrib.get('d')
+            yield child.attrib.get('id'), child.attrib.get('d')
 
-    def _parse_path(self, path: str) -> Shape:
-        current_coord = Coordinate(0,0)
+    def _parse_path(self, name:str, path: str) -> Shape:
+        """
+        This method takes a shape's path, as represented by a d tag in the an
+        svg file, and creates a PathReader instance to parse it.
+        """
+        pr = PathReader(name, path)
+        pr.parse_path()
+        return pr.to_shape()
         
-    def _is_case_one_match(self, path: str) -> Tuple[bool, int]:
-        """
-        This case returns a boolean indicating whether the next direction
-        is a reference (relative or absolute) to a specific coordinate. Also
-        returns the integer index for the end of this pattern.
-        """
-        pattern = r"[mM] ([\-0-9\.]+\,[0-9\.\-]+) "
-
-    def _is_case_two_match(self, path: str) -> Tuple[bool, int]:
-        """
-        This case returns a boolean indicating where the next direction is a
-        horizontal or vertical cut (relative or abosulte). Also returns the 
-        integer index of the end of the pattern.
-        """
-        pattern = r"[vVhH] (([0-9-+\.]+)|([0-9\.]+e\-[0-9]+)) "
-
-    def _is_case_three_match(self, path: str) -> Tuple[bool, int]:
-        """
-        This case returns true when the next pattern is a line to command
-        """
-        pattern = r"[lL] ([\-0-9\.]+(|e\-[0-9]))\,([0-9\.\-]+(|e\-[0-9])) "
 
 
 
-
-def parse(filepath: str) -> None:
-    pass 
 
 
     
 
 if __name__ == '__main__':
     folder = os.path.join(__parent_dir__, "test_files")
-    filename = "45_45_60_simplebox_01.svg"
+    filename = "Spice_rack_cut_01.svg"
 
     print("" == None)
 
-    SvgReader().get_shapes(os.path.join(folder, filename))
+    shape_arr = SvgReader().get_shapes(os.path.join(folder, filename))
+    print(shape_arr)
 
 
    
