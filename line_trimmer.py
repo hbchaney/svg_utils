ad96516eae055774c8fcb607cc9bf1671332ccaf
bxchaney <bxchaney@gmail.com>
2023-05-20 17:58:20 -0400

    removed whitespace

--- line_trimmer.py
+++ line_trimmer.py
@@ -3,7 +3,6 @@ main function for svg lib
 '''
 import sys
 
-
 def trim_svg(argv: list[str]) -> None: 
     #import all lower files 
     import logging
@@ -11,16 +10,12 @@ def trim_svg(argv: list[str]) -> None:
     if len(argv) >= 4 and argv[3] == '--debug': 
         logging.basicConfig(level=logging.DEBUG)
     else: 
-        logging.basicConfig(level=logging.INFO)
-        
-    processs_data(argv[1],argv[2])       
-
+        logging.basicConfig(level=logging.INFO)   
+    processs_data(argv[1],argv[2])    
 
 def processs_data(input_file : str, output_file : str) -> None: 
     from file.pathtools import read_paths
     read_paths(input_file,output_file)
-    
-    
+      
 if __name__ == '__main__': 
     trim_svg(sys.argv) 
-    
